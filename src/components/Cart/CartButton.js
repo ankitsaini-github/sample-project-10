0c45bffd88a17dd52fb282a2b0c09cb08bcc7d3c
ankitsaini-github <ankit121saini@gmail.com>
2024-02-03 19:55:32 +0530

    Doing Network calls and handling response

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,16 +1,18 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './CartButton.module.css';
 import { uiActions } from '../../store/uiSlice';
 
 const CartButton = (props) => {
   const dispatch=useDispatch()
+
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
   const showcarthandler=()=>{
     dispatch(uiActions.toggleCart())
   }
   return (
     <button className={classes.button} onClick={showcarthandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
